8396e44ea6da3578775a9a6ee39a781e1e8ebb8a
JanPieterCarp <jpott2000@gmail.com>
2024-02-25 15:41:55 +0100

    tmp

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\SessionController;
 use App\Http\Controllers\AdminController;
-use Nette\Utils\Json;
+use Tymon\JWTAuth\Http\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,32 +18,27 @@ use Nette\Utils\Json;
 */
 
 
-Route::get('/', [PostController::class, 'index'])->name('home')->middleware('guest')->middleware('throttle:5,1');
+//Route::get('/', [PostController::class, 'index'])->name('home')->middleware('guest')->middleware('throttle:5,1');
 
-Route::get('posts/{post:slug}', [PostController::class, 'show'])->where('posts','[A-z_\-]+')->middleware('throttle:5,1');
-
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest')->middleware('throttle:5,1');
-Route::post('register', [RegisterController::class, 'store'])->middleware('guest')->middleware('throttle:5,1');
-
-Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth')->middleware('throttle:5,1');
-
-Route::post('sessions', [SessionController::class, 'store'])->middleware('guest')->middleware('throttle:5,1');
-Route::get('login', [SessionController::class, 'create'])->middleware('guest')->middleware('throttle:5,1');
-
-Route::middleware('can:admin')->group(function () {
-
-    Route::post('admin/posts', [AdminController::class, 'store']);
-    Route::get('admin/posts/create', [AdminController::class, 'create']);
-    Route::get('admin/posts', [AdminController::class, 'index']);
-    Route::get('admin/posts/{post}/edit', [AdminController::class, 'edit']);
-    Route::patch('admin/posts/{post}', [AdminController::class, 'update']);
-    Route::delete('admin/posts/{post}', [AdminController::class, 'destroy']);
-
-})->middleware('throttle:5,1');
+Route::post('signup', [PostController::class, 'singup'])->middleware('throttle:5,1');
+Route::post('singupemployer', [PostController::class, 'signupEmployer'])->middleware('throttle:5,1');
+Route::get('me', [PostController::class, 'me'])->middleware('throttle:5,1')->middleware('jwt.auth');
+Route::post('logout', [PostController::class, 'logout'])->middleware('throttle:5,1')->middleware('jwt.auth');
 
 
-
-
-Route::middleware('auth:sanctum')->get('/user/{id}', function (Request $request) {
-    return $request->user();
+Route::prefix('post')->group(function () {
+Route::get('{post:slug}', [PostController::class, 'show'])->where('posts','[A-z_\-]+')->middleware('jwt.auth')->middleware('throttle:5,1');
+Route::post('create', [PostController::class, 'store'])->middleware('jwt.auth');
 });
+
+
+// Route::middleware('can:admin')->group(function () {
+
+//     Route::post('admin/posts', [AdminController::class, 'store']);
+//     Route::get('admin/posts/create', [AdminController::class, 'create']);
+//     Route::get('admin/posts', [AdminController::class, 'index']);
+//     Route::get('admin/posts/{post}/edit', [AdminController::class, 'edit']);
+//     Route::patch('admin/posts/{post}', [AdminController::class, 'update']);
+//     Route::delete('admin/posts/{post}', [AdminController::class, 'destroy']);
+
+// })->middleware('throttle:5,1');
