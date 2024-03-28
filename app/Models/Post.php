1644e20f6bdec7562f0a22ea704005d4696a0803
JanPieterCarp <jpott2000@gmail.com>
2024-03-28 16:22:59 +0100

    fix?

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,34 +10,18 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $gaurded = ['id'];
-
     protected $table = 'posts';
 
-
+    protected $hidden = [
+        'id',
+        'user_id',
+    ];
     protected $fillable = [
         'title',
         'body',
         'excerpt',
     ];
 
-    public function scopeFilter($query, array $filters){
-
-        $query->when($filters['search'] ?? false, fn($query, $search)=>
-            $query->where(fn($query)=>
-                $query->where('title', 'like', '%' . request('search') . '%')
-                    ->orwhere('body', 'like', '%' . request('search') . '%'))
-    );
-
-        $query->when($filters['category'] ?? false, fn($query, $category)=>
-            $query->whereHas('Category', fn ($query)=>
-                $query->where('slug', $category)));
-
-        $query->when($filters['author'] ?? false, fn($query, $author)=>
-            $query->whereHas('author', fn ($query)=>
-                $query->where('username', $author)));
-
-    }
 
     public function author()
     {
